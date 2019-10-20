e2dd1bd1d50c4ef5088f0f347672ac3ed5d787f3
Jesse Frohlich <jesse@frohli.ch>
2019-10-19 21:09:28 -0400

    Abstractify cameras; de-abstractify vectors; introduce antialiasing
    Antialiasing is implemented by uniformly sampling each pixel. I am not
    using random sampling because it is
    a) one more thing to implement and
    b) not entirely necessary yet
    
    Compilations are becoming considerably slower; especially when the
    number of rays cast increases by a factor of 16. I will eventually need
    to find a bottleneck when compilation becomes far too slow.

--- image.hs
+++ image.hs
@@ -2,39 +2,35 @@ import System.Environment
 import Linear
 import Control.Lens
 
-type Vec   = V3 Double
-type Color = V3 Int
+type Color = V3 Double
 
 data Ray = Ray
-  {orig :: Vec
-  ,dir  :: Vec
+  {orig :: V3 Double
+  ,dir  :: V3 Double
   } deriving (Show)
 
 infinity :: Double
 infinity = (read "Infinity")::Double
 
-pointAtParam :: Ray -> Double -> Vec
+pointAtParam :: Ray -> Double -> V3 Double
 pointAtParam r t = orig r + t*^(dir r)
 
 -- Helper functions
 makePixel :: Color -> String
-makePixel (V3 r g b) = show r ++ " " ++ show g ++ " " ++ show b ++ "\n"
+makePixel v = show r ++ " " ++ show g ++ " " ++ show b ++ "\n"
+  where (V3 r g b) = fromVec v
 
 rescale :: Double -> Int
 rescale = floor . (255.99*)
 
-fromVec :: Vec -> Color
+fromVec :: V3 Double -> V3 Int
 fromVec = fmap rescale
-
-{-
-  We want an object to be hittable.
- -}
 
 data HitRecord = Miss
                | HitRecord
                  {time :: Double
-                 ,pt :: Vec
-                 ,normal :: Vec
+                 ,pt :: V3 Double
+                 ,normal :: V3 Double
                  } deriving (Eq)
 
 instance Ord HitRecord where
@@ -43,27 +39,26 @@ instance Ord HitRecord where
   HitRecord _ _ _ `compare` Miss            = LT
   h1              `compare` h2              = (time h1) `compare` (time h2)
 
-class Object a where
+class EMO a where -- ElectroMagnetic Object
   hit :: Ray
       -> Double    -- tmin
       -> Double    -- tmax
       -> a
       -> HitRecord
 
-instance (Object a) => Object [a] where
-  hit r tmin tmax o =
-    -- TODO: tmax below should depend on how many objects have been hit so far.
-    let hs = map (hit r tmin tmax) o
-     in case hs of
-          [] -> Miss
-          _  -> foldr1 min hs
+instance (EMO a) => EMO [a] where
+  hit r tmin tmax os = foldr (\o h->
+    case h of
+      Miss -> hit r tmin tmax o
+      _    -> min h $ hit r tmin (time h) o
+                             ) Miss os
 
 data Sphere = Sphere
-  {centre :: Vec
+  {centre :: V3 Double
   ,radius :: Double
   } deriving (Show, Eq)
 
-instance Object Sphere where
+instance EMO Sphere where
   hit r tmin tmax s =
     let
       k = orig r - (centre s)
@@ -87,41 +82,83 @@ instance Object Sphere where
                     in HitRecord x2 p (signorm (p - (centre s)))
               else Miss
 
+data Camera = Camera
+  {lowerLeftCorner :: V3 Double
+  ,horizontal :: V3 Double
+  ,vertical :: V3 Double
+  ,origin :: V3 Double
+  ,width :: !Int
+  ,height :: !Int
+  }
+
+getRay :: Camera -> V2 Double -> Ray
+getRay c (V2 u v) = Ray
+  {orig = origin c
+  ,dir = lowerLeftCorner c + u*^horizontal c + v*^vertical c - origin c
+  }
+
+ -- Extraneous function
+getRayInt :: Camera -> V2 Int -> Ray
+getRayInt c (V2 i j) = Ray
+  {orig = origin c
+  ,dir = lowerLeftCorner c
+       + (fromIntegral i/(fromIntegral $ width c))*^horizontal c
+       + (fromIntegral j/(fromIntegral $ height c))*^vertical c - origin c
+  }
+
 -- Practice functions
 
-color :: (Object a) => Ray -> a -> Color
-color r o =
+color :: (EMO a) => a -> Ray -> Color
+color o r =
   case hit r 0 infinity o of
-    Miss -> fromVec $ lerp s v w
-    h    -> fromVec $ 0.5*^ (normal h + V3 1 1 1)
+    Miss -> lerp s v w
+    h    -> 0.5*^ (normal h + V3 1 1 1)
   where -- background gradient
     s = 0.5*(1-((dir r) ^. _y))
     v = V3 1 1 1
     w = V3 0.5 0.7 1
 
-makePicture :: Int -> Int -> String
-makePicture w h = header ++ body
+perturb :: (Camera -> V2 Double -> Color) -- raycaster
+        -> Camera
+        -> V2 Double           -- pixel screen coordinate
+        -> [V2 Double]         -- list of pixel perturbations
+        -> Color
+perturb f ca co ps
+  = recip (fromIntegral $ length ps) *^ (sum $ fmap (f ca . (+co).
+    (\(V2 u v )->
+      V2 (u/(fromIntegral $ width ca)) (v/(fromIntegral $ height ca)))) ps)
+
+makePicture :: Int -> Int -> Int -> String
+makePicture w h aa = header ++ body
   where
     header = "P3\n"
       ++ show w ++ " "
       ++ show h ++ "# width height (pixels)\n"
       ++ "255 # max intensity\n\n"
-    lowerLeftCorner = V3 (-2) (-1) (-1)
-    horizontal = V3 4 0 0
-    vertical = V3 0 2 0
+    cam = Camera
+      {lowerLeftCorner = V3 (-2) (-1) (-1)
+      ,horizontal = V3 4 0 0
+      ,vertical = V3 0 2 0
+      ,origin = zero
+      ,width = w
+      ,height = h
+      }
     world =
       [Sphere (V3 0 0 (-1)) 0.5
-      , Sphere (V3 0 (-100.5) (-1)) 100
+      ,Sphere (V3 0 (-100.5) (-1)) 100
       ]
     body = concat $ do
       j <- map fromIntegral [h-1,h-2..0]
       i <- map fromIntegral [0..w-1]
-      let u = i/(fromIntegral w)
-      let v = j/(fromIntegral h)
-      let r = Ray {orig=zero, dir= lowerLeftCorner + u*^horizontal + v*^vertical}
-      let c = color r world
-      return $ makePixel c
+      let n = fromIntegral aa
+      let jigs = [V2 (k/n)(l/n) | k<-[0..n-1],l<-[0..n-1]]
+      let u = fromIntegral i
+      let v = fromIntegral j
+      let pos = V2 (u/(fromIntegral w)) (v/(fromIntegral h))
+      return . makePixel $ perturb (
+        \ca co -> color world (getRay ca co)
+                                   ) cam pos jigs
 
 main = do
   n <- getProgName
-  writeFile (n++".ppm") $ makePicture 200 100
+  writeFile (n++".ppm") $ makePicture 400 200 4
